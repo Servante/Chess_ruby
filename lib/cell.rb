1ba071351746ed0fb1638d057b202b5e5bcb9887
Wesley <matthew282@msn.com>
2022-02-22 12:27:36 -0700

    added class Cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+class Cell
+	attr_accessor :value, :stackable
+
+	def initialize(value = "-", stackable = false)
+		@value = value
+		@stackable = stackable
+	end
+end
