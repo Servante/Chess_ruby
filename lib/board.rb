2a41644b01e9920b1b9a9e7f2f570e08c67f2d7f
Wesley <matthew282@msn.com>
2022-02-28 13:35:35 -0700

    updated board heredoc

--- lib/board.rb
+++ lib/board.rb
@@ -20,20 +20,24 @@ class Board
 		puts <<-HEREDOC
 
 		
-		---+----+----+----+----+----+----
-		 #{cells[1][0].value} |  #{cells[1][1].value} |  #{cells[1][2].value} |  #{cells[1][3].value} |  #{cells[1][4].value} |  #{cells[1][5].value} |  #{cells[1][6].value}
-		---+----+----+----+----+----+----
-		 #{cells[2][0].value} |  #{cells[2][1].value} |  #{cells[2][2].value} |  #{cells[2][3].value} |  #{cells[2][4].value} |  #{cells[2][5].value} |  #{cells[2][6].value}
-		---+----+----+----+----+----+----
-		 #{cells[3][0].value} |  #{cells[3][1].value} |  #{cells[3][2].value} |  #{cells[3][3].value} |  #{cells[3][4].value} |  #{cells[3][5].value} |  #{cells[3][6].value}
-		---+----+----+----+----+----+----
-		 #{cells[4][0].value} |  #{cells[4][1].value} |  #{cells[4][2].value} |  #{cells[4][3].value} |  #{cells[4][4].value} |  #{cells[4][5].value} |  #{cells[4][6].value}
-		---+----+----+----+----+----+----
-		 #{cells[5][0].value} |  #{cells[5][1].value} |  #{cells[5][2].value} |  #{cells[5][3].value} |  #{cells[5][4].value} |  #{cells[5][5].value} |  #{cells[5][6].value}
-		---+----+----+----+----+----+----
-		 #{cells[6][0].value} |  #{cells[6][1].value} |  #{cells[6][2].value} |  #{cells[6][3].value} |  #{cells[6][4].value} |  #{cells[6][5].value} |  #{cells[6][6].value}
-		---+----+----+----+----+----+----
-		 A |  B |  C |  D |  D |  F |  G
+		  +---+----+----+----+----+----+----+----+
+		8 | #{cells[1][0].value} |  #{cells[1][1].value} |  #{cells[1][2].value} |  #{cells[1][3].value} |  #{cells[1][4].value} |  #{cells[1][5].value} |  #{cells[1][6].value} | #{cells[1][7].value}  | 
+		  +---+----+----+----+----+----+----+----+
+		7 | #{cells[2][0].value} |  #{cells[2][1].value} |  #{cells[2][2].value} |  #{cells[2][3].value} |  #{cells[2][4].value} |  #{cells[2][5].value} |  #{cells[2][6].value} | #{cells[2][7].value}  |
+		  +---+----+----+----+----+----+----+----+
+		6 | #{cells[3][0].value} |  #{cells[3][1].value} |  #{cells[3][2].value} |  #{cells[3][3].value} |  #{cells[3][4].value} |  #{cells[3][5].value} |  #{cells[3][6].value} | #{cells[3][7].value}  |
+		  +---+----+----+----+----+----+----+----+
+		5 | #{cells[4][0].value} |  #{cells[4][1].value} |  #{cells[4][2].value} |  #{cells[4][3].value} |  #{cells[4][4].value} |  #{cells[4][5].value} |  #{cells[4][6].value} | #{cells[4][7].value}  |
+		  +---+----+----+----+----+----+----+----+
+		4 | #{cells[5][0].value} |  #{cells[5][1].value} |  #{cells[5][2].value} |  #{cells[5][3].value} |  #{cells[5][4].value} |  #{cells[5][5].value} |  #{cells[5][6].value} | #{cells[5][7].value}  |
+		  +---+----+----+----+----+----+----+----+
+		3 | #{cells[6][0].value} |  #{cells[6][1].value} |  #{cells[6][2].value} |  #{cells[6][3].value} |  #{cells[6][4].value} |  #{cells[6][5].value} |  #{cells[6][6].value} | #{cells[6][7].value}  |
+		  +---+----+----+----+----+----+----+----+
+		2 | #{cells[7][0].value} |  #{cells[7][1].value} |  #{cells[7][2].value} |  #{cells[7][3].value} |  #{cells[7][4].value} |  #{cells[7][5].value} |  #{cells[7][6].value} | #{cells[7][7].value}  |
+		  +---+----+----+----+----+----+----+----+
+		1 | #{cells[8][0].value} |  #{cells[8][1].value} |  #{cells[8][2].value} |  #{cells[8][3].value} |  #{cells[8][4].value} |  #{cells[8][5].value} |  #{cells[8][6].value} | #{cells[8][7].value}  |
+		  +---+----+----+----+----+----+----+----+
+		  | A |  B |  C |  D |  D |  F |  G |  H |
 
 		HEREDOC
 
@@ -60,7 +64,7 @@ class Board
 
 	def create_cell(counter)
 		if counter == 6
-			new_cell = Cell.new("-", true)
+			new_cell = Cell.new(" ", true)
 		elsif
 			new_cell = Cell.new
 		end
