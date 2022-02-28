9505512403a8537f7c23013dc09cc2dcc650cce5
Wesley <matthew282@msn.com>
2022-02-28 14:35:51 -0700

    added #game_setup

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
+require_relative 'display.rb'
 
 class Game
+	include Display
 	attr_accessor :player1, :player2, :current_player, :board
 
 	def initalize 
@@ -10,4 +12,15 @@ class Game
 		@board = Board.new
 	end
 
-	
+	private
+
+	def game_setup
+		display_introduction
+		@player1 = player_creation
+		@player2 = player_creation
+	end
+
+end
+
+
+
