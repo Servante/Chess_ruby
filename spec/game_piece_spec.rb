ecb58b7ee03f8739052e312bc6ed7fdef6b9b9cf
Wesley <matthew282@msn.com>
2022-03-03 13:22:46 -0700

    added game_piece_spec.rb

---
+++ spec/game_piece_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require '../lib/game.rb'
+require '../lib/board.rb'
+require '../lib/cell.rb'
+require '../lib/player.rb'
+require '../lib/display.rb'
+require '../lib/pieces.rb'
+require 'pry'
+
+
